2298f13b05e88bfba81018bde210c55f9d8b7fc7
Kevin Viers <kevin@beans.llc>
2023-04-23 17:50:18 -0700

    Fixed search not working when inputting the desired zip.
    Warning: this app is so ugly that even its mother (me) could not love it. It needs a major makeover.

--- app/src/main/java/com/C2PG/Weather254/MainActivity.kt
+++ app/src/main/java/com/C2PG/Weather254/MainActivity.kt
@@ -2,6 +2,7 @@ package com.C2PG.Weather254
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.C2PG.Weather254.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -16,11 +17,14 @@ private const val bURL = "https://api.weatherapi.com/v1/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var currentTempTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        currentTempTextView = binding.currentTempTextView
 
         // Set a click listener on the search button
         binding.searchButton.setOnClickListener {
@@ -46,43 +50,34 @@ class MainActivity : AppCompatActivity() {
 
         retrofitData.enqueue(object: Callback<weatherData>
         {
-            override fun onResponse(call: Call<weatherData>, response: Response<weatherData>)
-            {
+            override fun onResponse(call: Call<weatherData>, response: Response<weatherData>) {
                 if (response.isSuccessful && !game) {
-                    //Variable that contains all data
                     val responseBody = response.body()!!
-                    showData(responseBody=responseBody)
-                }
-                else if (response.isSuccessful && game)
-                {
+                    showData(responseBody)
+                } else if (response.isSuccessful && game) {
                     val temp = response.body()!!.current.temp_f
-                    binding.currentTempTextView.text = temp.toString() + "°F"
-                }
-                else if (!response.isSuccessful && game)
-                {
+                    currentTempTextView.text = temp.toString() + "°F"
+                } else if (!response.isSuccessful && game) {
                     randZip()
-                }
-                else
-                {
+                } else {
                     Toast.makeText(applicationContext, "Zipcode Error", Toast.LENGTH_LONG).show()
                 }
-
             }
+
             override fun onFailure(call: Call<weatherData>, t:Throwable)
             {
                 Toast.makeText(applicationContext, "Call Error, Check connection", Toast.LENGTH_LONG).show()
             }
         })
     }
+
     private fun showData(responseBody: weatherData) {
         binding.headerLocationName.text = responseBody.location.name
-        binding.currentTempTextView.text = "${responseBody.current.temp_f}°F"
+        currentTempTextView.text = "${responseBody.current.temp_f}°F"
     }
 
-
-    //call twice when game is clicked to generate two zips
-    private fun randZip()
-    {
+    // Call twice when game is clicked to generate two zips
+    private fun randZip() {
         val random = Random
         val zipCode = random.nextInt(99999 - 10000) + 10000
         val newZipCode = String.format("%05d", zipCode)
