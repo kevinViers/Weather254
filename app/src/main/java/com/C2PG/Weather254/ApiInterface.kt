c770b04de2c83996b2c5a46c2017594b52110a19
Kevin Viers <kevin@beans.llc>
2023-04-24 17:09:22 -0700

    made app easier to read and interact with with regards to color of the text and search button

--- app/src/main/java/com/C2PG/Weather254/ApiInterface.kt
+++ app/src/main/java/com/C2PG/Weather254/ApiInterface.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val key = "627b37bf9c7949a2a8042815231104"
-public interface ApiInterface {
+interface ApiInterface {
     //Query takes a parameter which should match links query, and takes in a variable
     //Query output -> "&parameter=variable" into URL, query placement in URL doesn't matter
 @GET("forecast.json?key=627b37bf9c7949a2a8042815231104&days=3&aqi=yes")
