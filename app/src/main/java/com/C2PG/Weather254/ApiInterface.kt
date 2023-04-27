eece6811c0cf55584ea465da97062042dc05a991
Josue <xJaramis@gmail.com>
2023-04-26 20:38:06 -0700

    Small Change
    New key unnecessary, woops

--- app/src/main/java/com/C2PG/Weather254/ApiInterface.kt
+++ app/src/main/java/com/C2PG/Weather254/ApiInterface.kt
@@ -4,8 +4,8 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val keyForecast = "627b37bf9c7949a2a8042815231104"
-private const val keyCurrent = "302baf19e4d14533b2130228232704"
+private const val key = "627b37bf9c7949a2a8042815231104"
+
 interface ApiInterface {
     //Query takes a parameter which should match links query, and takes in a variable
     //Query output -> "&parameter=variable" into URL, query placement in URL doesn't matter
@@ -13,7 +13,7 @@ interface ApiInterface {
 fun getForecastData(@Query("q") location:String): Call<weatherData>
 
 //Used to return only super basic information for game (current temp of location)
-@GET("current.json?key=302baf19e4d14533b2130228232704")
+@GET("current.json?key=627b37bf9c7949a2a8042815231104")
     fun getCurrentData(@Query("q") location:String) : Call<currentGameData>
 }
 
